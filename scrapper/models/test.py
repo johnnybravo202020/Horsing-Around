2e0f601b8379c6f4ecded3f497249f70c9ba6760
home-pc-egeaydin <egeaydin@gmail.com>
2017-10-06 17:37:31 -0700

    fixed a bug that was breaking web tests

--- scrapper/models/test.py
+++ scrapper/models/test.py
@@ -4,11 +4,14 @@ from scrapper.managers import TestDataManager
 from scrapper.scrappers import City, PageType
 from .mixin import ResultMixin
 
+
 class RaceDayTestData(models.Model):
     """
     Will store the entire html source code of a particular race day for the purpose of making unit tests faster
     rather than waiting for the page to be downloaded.
     """
+    objects = TestDataManager()
+
     html_source = models.TextField()
     url = models.CharField(max_length=200)
     city_id = models.IntegerField(default=0)
