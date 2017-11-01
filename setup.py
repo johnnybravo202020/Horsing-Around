e98a7b428cb6e16b8409716f13769cf579c0d3f9
home-pc-egeaydin <egeaydin@gmail.com>
2017-11-01 08:13:36 -0700

    a

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-horsing_around',
     version='0.1.2',
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     description='Scraps data from the official web site of the horse races run in Turkey, in order to forecast the race results',
     long_description=README,
